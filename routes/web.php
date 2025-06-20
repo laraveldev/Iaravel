4daef778a29291ab4e16715f0b10c9bb56b7d126
laraveldev <elnurbek.pubgmobile@gmail.com>
2025-06-20 19:54:31 +0500

    PATCH: add routes for view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/venues', function () {
+    return view('venues');
+});
+
+Route::get('/services', function () {
+    return view('services');
+});
+
+Route::get('/books', function () {
+    return view('books');
 });
